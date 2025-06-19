19855f3125e808180b578ac81d797e19c2636bce
Valeria <kguamanleon03@est.ups.edu.ec>
2025-06-19 09:13:14 -0500

    Arbol Binario Atributo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import Materia.Controllers.ArbolBinario;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        ArbolBinario ab = new ArbolBinario();
+
+    
+        ab.insert(50);
+        ab.insert(17);
+        ab.insert(76);
+        ab.insert(9);
+        ab.insert(23);
+        ab.insert(54);
+        ab.insert(14);
+        ab.insert(19);
+
+      
+        System.out.println("Nombre: Valeria Guaman\n");
+
+        System.out.println("Peso del arbol = " + ab.getSize());
+        System.out.println("Altura es = " + ab.getHeight() + "\n");
+
+      
+        System.out.println("Arbol InOrder");
+        ab.printInOrderSimple();
+
+        System.out.println("Arbol InOrder con alturas");
+        ab.printInOrderWithHeights();
+
+        System.out.println("Arbol InOrder con factor de equilibrio");
+        ab.printInOrderWithBalance();
+
+      
+        System.out.println("Arbol esta equilibrado = " + ab.estaEquilibrado());
+
+        int nuevo = 15;
+        System.out.println("Agregamos valor = " + nuevo);
+        ab.insert(nuevo);
+
+     
+        System.out.println("Arbol InOrder con factor de equilibrio");
+        ab.printInOrderWithBalance();
+
+        System.out.print("Nodos desequilibrados ");
+        ab.printNodosDesequilibrados();
     }
 }
---
+++ src/Materia/Controllers/ArbolBinario.java
@@ -0,0 +1,147 @@
+package Materia.Controllers;
+
+import Materia.models.Node;
+
+public class ArbolBinario {
+    private Node root;
+    private int size;
+
+    public ArbolBinario() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    private Node insertRec(Node padre, int value) {
+        if (padre == null) {
+            size++;
+            return new Node(value);
+        }
+        if (value < padre.getValue()) {
+            padre.setLeft(insertRec(padre.getLeft(), value));
+        } else if (value > padre.getValue()) {
+            padre.setRight(insertRec(padre.getRight(), value));
+        }
+        return padre;
+    }
+
+    public void imprimirArbol() {
+        imprimir(root);
+        System.out.println();
+    }
+
+    private void imprimir(Node node) {
+        if (node != null) {
+            System.out.print(node.getValue() + " , ");
+            imprimir(node.getLeft());
+            imprimir(node.getRight());
+        }
+    }
+
+    public boolean buscar(int value) {
+        return buscarRec(root, value);
+    }
+
+    private boolean buscarRec(Node node, int value) {
+        if (node == null) return false;
+        if (node.getValue() == value) return true;
+
+        if (value < node.getValue()) {
+            return buscarRec(node.getLeft(), value);
+        } else {
+            return buscarRec(node.getRight(), value);
+        }
+    }
+
+    public int getHeight() {
+        return getHeight(root);
+    }
+
+    private int getHeight(Node node) {
+        if (node == null) return 0;
+        int leftHeight = getHeight(node.getLeft());
+        int rightHeight = getHeight(node.getRight());
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    // InOrder simple (solo valores)
+    public void printInOrderSimple() {
+        printInOrderSimple(root);
+        System.out.println();
+    }
+
+    private void printInOrderSimple(Node node) {
+        if (node != null) {
+            printInOrderSimple(node.getLeft());
+            System.out.print(node.getValue() + " ");
+            printInOrderSimple(node.getRight());
+        }
+    }
+
+    // InOrder con alturas
+    public void printInOrderWithHeights() {
+        printInOrderWithHeights(root);
+        System.out.println();
+    }
+
+    private void printInOrderWithHeights(Node node) {
+        if (node != null) {
+            printInOrderWithHeights(node.getLeft());
+            System.out.print(node.getValue() + " (h = " + getHeight(node) + ") ");
+            printInOrderWithHeights(node.getRight());
+        }
+    }
+
+    public void printInOrderWithBalance() {
+        printInOrderWithBalance(root);
+        System.out.println();
+    }
+
+    private void printInOrderWithBalance(Node node) {
+        if (node != null) {
+            printInOrderWithBalance(node.getLeft());
+            System.out.print(node.getValue() + " (bf = " + getBalanceRec(node) + ") ");
+            printInOrderWithBalance(node.getRight());
+        }
+    }
+
+    private int getBalanceRec(Node node) {
+        if (node == null) return 0;
+        return getHeight(node.getLeft()) - getHeight(node.getRight());
+    }
+
+    public boolean estaEquilibrado() {
+        return estaEquilibrado(root);
+    }
+
+    private boolean estaEquilibrado(Node node) {
+        if (node == null) return true;
+        int balance = getBalanceRec(node);
+        if (Math.abs(balance) > 1) return false;
+        return estaEquilibrado(node.getLeft()) && estaEquilibrado(node.getRight());
+    }
+
+    public void printNodosDesequilibrados() {
+    printNodosDesequilibrados(root);
+    System.out.println();
+}
+
+private void printNodosDesequilibrados(Node node) {
+    if (node != null) {
+        printNodosDesequilibrados(node.getLeft());
+        int bf = getBalanceRec(node);
+        if (Math.abs(bf) > 1) {
+            System.out.print(node.getValue() + "(fE = " + bf + ") ");
+        }
+        printNodosDesequilibrados(node.getRight());
+    }
+}
+
+}
